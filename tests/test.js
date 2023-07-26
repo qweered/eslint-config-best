cbe255eb652b43d123aab27b80df796638fc6437
qweered <grubian2@gmail.com>
2023-07-26 22:37:33 +0300

    cleanup tests, remove readme

---
+++ tests/test.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const require = createRequire(import.meta.url);
+
+/* eslint-env node */
+/*
+  eslint-disable
+  import/max-dependencies
+*/
+import("../base.json");
+import("../prettier.json");
+import("../index.json");
+import("../fp.json");
+import("../node.json");
+import("../ts-for-js.json");
+import("../ts.json");
+import("../react.json");
+import("../react-testing-library.json");
+import("../jest.json");
+import("../tsconfig.json");
+import("../disabled-rules.json");
+import("../rules-disabled-by-prettier.json");
+
