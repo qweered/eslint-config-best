e33f3407fafbb006e589a98a5125b0f140cb8de4
qweered <grubian2@gmail.com>
2023-07-26 22:21:29 +0300

    init, remove everything about vue and nuxt

---
+++ esm/test-jsx.jsx
@@ -0,0 +1,3 @@
+// eslint-disable-next-line max-len
+// eslint-disable-next-line import/no-anonymous-default-export,import/no-unused-modules
+export default "test";
---
+++ test.js
@@ -0,0 +1,24 @@
+/* eslint-env node */
+
+/*
+  eslint-disable
+  unicorn/prefer-module,
+  putout/putout,
+  import/max-dependencies
+*/
+
+"use strict";
+
+require("./base.json");
+require("./prettier.json");
+require("./index.json");
+require("./fp.json");
+require("./node.json");
+require("./ts-for-js.json");
+require("./ts.json");
+require("./react.json");
+require("./react-testing-library.json");
+require("./jest.json");
+require("./tsconfig.json");
+require("./disabled-rules.json");
+require("./rules-disabled-by-prettier.json");
